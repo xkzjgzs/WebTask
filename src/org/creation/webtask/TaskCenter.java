9ccae9a9badafe8d55ccb9ed6a1bb7fcf8af2580
xkzjgzs <xkzjgzs@126.com>
2014-05-13 13:42:58 +0800

    Signed-off-by: xkzjgzs <xkzjgzs@126.com>

---
+++ src/org/creation/webtask/TaskCenter.java
@@ -0,0 +1,13 @@
+package org.creation.webtask;
+
+public class TaskCenter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
